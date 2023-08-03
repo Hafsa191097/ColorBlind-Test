ab5b62bfed5208aad40291bbc56b70887f91d4cf
Hafsa19197 <hafsaayyhere@gmail.com>
2023-08-03 10:41:07 -0700

    last

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,20 @@ import 'dart:async';
 import 'package:color_blind_test/pre_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  WidgetsBinding binding=WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: binding);
+  WidgetsFlutterBinding.ensureInitialized();
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
-  FlutterNativeSplash.remove();
+}
+
+Future initialization(BuildContext? context) async {
+  await Future.delayed(const Duration(seconds:3));
 }
 
 class MyApp extends StatelessWidget {
@@ -33,4 +35,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
